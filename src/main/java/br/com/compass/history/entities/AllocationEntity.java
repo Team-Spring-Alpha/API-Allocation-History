215b85bff1671d4900bead5784e6bb5c8f2a1e79
Heloisa Hobold Barbosa <heloisa.barbosa.pb@compasso.com.br>
2022-09-01 14:53:34 -0400

    criação do endpoint GET para listagem de alocações

---
+++ src/main/java/br/com/compass/history/entities/AllocationEntity.java
@@ -0,0 +1,21 @@
+package br.com.compass.history.entities;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "Allocation")
+@Data
+public class AllocationEntity {
+
+    private String userId;
+    @Id
+    private String allocationNumber;
+    private int cardNumber;
+    private List<AllocationMovieEntity> movies;
+    private String paymentStatus;
+    private LocalDateTime allocationDate = LocalDateTime.now();
+}
---
+++ src/main/java/br/com/compass/history/service/AllocationHistoryService.java
@@ -0,0 +1,31 @@
+package br.com.compass.history.service;
+
+import br.com.compass.history.dto.response.ResponseAllocation;
+import br.com.compass.history.entities.AllocationEntity;
+import br.com.compass.history.repository.AllocationRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AllocationHistoryService {
+
+    @Autowired
+    private AllocationRepository repository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+    
+
+    public List<ResponseAllocation> findByUserId(String userId){
+        List<AllocationEntity> allocationEntities = repository.findByUserId(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        List<ResponseAllocation> collect = allocationEntities.stream().map(allocationEntity ->
+                modelMapper.map(allocationEntity, ResponseAllocation.class)).collect(Collectors.toList());
+        return collect;
+    }
+}
