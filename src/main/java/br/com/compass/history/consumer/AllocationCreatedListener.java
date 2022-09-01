9b8da8c86ce9e928e4961611e59a55941cb146fd
Heloisa Hobold Barbosa <heloisa.barbosa.pb@compasso.com.br>
2022-09-01 15:57:35 -0400

    fix consumer

--- src/main/java/br/com/compass/history/consumer/AllocationCreatedListener.java
+++ src/main/java/br/com/compass/history/consumer/AllocationCreatedListener.java
@@ -1,6 +1,7 @@
-package br.com.compass.history.service;
+package br.com.compass.history.consumer;
 
 import br.com.compass.history.dto.request.RequestAllocation;
+import br.com.compass.history.service.AllocationHistoryService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
