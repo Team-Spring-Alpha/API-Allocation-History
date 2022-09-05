8479151bc019e611c8dfe747974d021333caeb63
Heloisa Hobold Barbosa <heloisa.barbosa.pb@compasso.com.br>
2022-09-05 16:00:15 -0400

    aplicando validação, renomeando pacote e atualizando caminho

--- src/main/java/br/com/compass/history/dto/request/RequestAllocationMovie.java
+++ src/main/java/br/com/compass/history/dto/request/RequestAllocationMovie.java
@@ -2,8 +2,13 @@ package br.com.compass.history.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class RequestAllocationMovie {
+    @NotNull
     private Long id;
+    @NotBlank
     private String name;
 }
