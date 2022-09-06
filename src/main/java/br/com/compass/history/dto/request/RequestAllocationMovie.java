850fb8513cc290a1d140f943008eba34ea748bfc
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-09-06 10:39:01 -0300

    fix conflict

--- src/main/java/br/com/compass/history/dto/request/RequestAllocationMovie.java
+++ src/main/java/br/com/compass/history/dto/request/RequestAllocationMovie.java
@@ -1,11 +1,13 @@
 package br.com.compass.history.dto.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
 public class RequestAllocationMovie {
     @NotNull
     private Long id;
