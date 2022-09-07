e4207f7622ad1a1d8f14e9388d5cb56df375bb00
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-09-07 13:01:46 -0300

    Fix: modelMapper erro + sort by date
    Por falta do NoArgsConstructor o modelMapper não estava funcionando

--- src/main/java/br/com/compass/history/dto/request/RequestAllocationMovie.java
+++ src/main/java/br/com/compass/history/dto/request/RequestAllocationMovie.java
@@ -1,13 +1,17 @@
 package br.com.compass.history.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestAllocationMovie {
     @NotNull
     private Long id;
--- src/main/java/br/com/compass/history/entities/AllocationMovieEntity.java
+++ src/main/java/br/com/compass/history/entities/AllocationMovieEntity.java
@@ -1,10 +1,14 @@
 package br.com.compass.history.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AllocationMovieEntity {
     private Long id;
     private String name;
